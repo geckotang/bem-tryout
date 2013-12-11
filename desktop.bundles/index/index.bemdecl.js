8dc26bb10fad588ca64b6fd1b9b14a3a666b461f
geckotang <macky1207@gmail.com>
2013-12-12 01:50:47 +0900

    BEMツール(4)の分を追加
    生成したファイルもpushするようにした。

---
+++ desktop.bundles/index/index.bemdecl.js
@@ -0,0 +1,77 @@
+exports.blocks = [
+    {
+        "name": "page",
+        "elems": [
+            {
+                "name": "css"
+            },
+            {
+                "name": "meta"
+            },
+            {
+                "name": "js"
+            }
+        ]
+    },
+    {
+        "name": "header"
+    },
+    {
+        "name": "layout",
+        "elems": [
+            {
+                "name": "left"
+            },
+            {
+                "name": "right"
+            }
+        ]
+    },
+    {
+        "name": "b-search",
+        "elems": [
+            {
+                "name": "input"
+            },
+            {
+                "name": "button"
+            }
+        ]
+    },
+    {
+        "name": "b-logo",
+        "elems": [
+            {
+                "name": "link"
+            },
+            {
+                "name": "icon"
+            },
+            {
+                "name": "slogan"
+            }
+        ]
+    },
+    {
+        "name": "heroImage"
+    },
+    {
+        "name": "tab",
+        "elems": [
+            {
+                "name": "item"
+            }
+        ],
+        "mods": [
+            {
+                "name": "is",
+                "vals": [
+                    "current"
+                ]
+            }
+        ]
+    },
+    {
+        "name": "goods"
+    }
+];
---
+++ desktop.bundles/index/index.deps.js
@@ -0,0 +1,308 @@
+exports.deps = [
+    {
+        "block": "i-bem"
+    },
+    {
+        "block": "inherit"
+    },
+    {
+        "block": "identify"
+    },
+    {
+        "block": "next-tick"
+    },
+    {
+        "block": "objects"
+    },
+    {
+        "block": "functions"
+    },
+    {
+        "block": "events"
+    },
+    {
+        "block": "events",
+        "elem": "channels"
+    },
+    {
+        "block": "ecma",
+        "elem": "object"
+    },
+    {
+        "block": "ecma",
+        "elem": "array"
+    },
+    {
+        "block": "ecma",
+        "elem": "function"
+    },
+    {
+        "block": "page"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom"
+    },
+    {
+        "block": "jquery"
+    },
+    {
+        "block": "loader"
+    },
+    {
+        "block": "loader",
+        "mod": "type"
+    },
+    {
+        "block": "loader",
+        "mod": "type",
+        "val": "js"
+    },
+    {
+        "block": "jquery",
+        "elem": "config"
+    },
+    {
+        "block": "dom"
+    },
+    {
+        "block": "i-bem",
+        "elem": "internal"
+    },
+    {
+        "block": "ecma"
+    },
+    {
+        "block": "ecma",
+        "elem": "string"
+    },
+    {
+        "block": "ecma",
+        "elem": "json"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom",
+        "mod": "init"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom",
+        "mod": "init",
+        "val": "auto"
+    },
+    {
+        "block": "ua"
+    },
+    {
+        "block": "page",
+        "elem": "css"
+    },
+    {
+        "block": "page",
+        "elem": "js"
+    },
+    {
+        "block": "page",
+        "elem": "meta"
+    },
+    {
+        "block": "header"
+    },
+    {
+        "block": "layout"
+    },
+    {
+        "block": "layout",
+        "elem": "left"
+    },
+    {
+        "block": "layout",
+        "elem": "right"
+    },
+    {
+        "block": "b-search"
+    },
+    {
+        "block": "b-layout-table"
+    },
+    {
+        "block": "b-search",
+        "elem": "layout"
+    },
+    {
+        "block": "b-search",
+        "elem": "input"
+    },
+    {
+        "block": "b-search",
+        "elem": "button"
+    },
+    {
+        "block": "i-bem",
+        "elem": "html"
+    },
+    {
+        "block": "b-link"
+    },
+    {
+        "block": "b-icon"
+    },
+    {
+        "block": "b-logo"
+    },
+    {
+        "block": "b-logo",
+        "elem": "link"
+    },
+    {
+        "block": "b-logo",
+        "elem": "icon"
+    },
+    {
+        "block": "b-logo",
+        "elem": "slogan"
+    },
+    {
+        "block": "heroImage"
+    },
+    {
+        "block": "tab"
+    },
+    {
+        "block": "tab",
+        "elem": "item"
+    },
+    {
+        "block": "tab",
+        "mod": "is"
+    },
+    {
+        "block": "tab",
+        "mod": "is",
+        "val": "current"
+    },
+    {
+        "block": "goods"
+    }
+];
+exports.depsByTechs = {
+    "": {},
+    "test.js": {
+        "bemhtml": [
+            {
+                "tech": "bemhtml",
+                "block": "i-bem"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "inherit"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "identify"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "next-tick"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "objects"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "functions"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "events"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "events",
+                "elem": "channels"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "ecma",
+                "elem": "object"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "ecma",
+                "elem": "array"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "ecma",
+                "elem": "function"
+            }
+        ]
+    },
+    "bemhtml": {
+        "bemhtml": [
+            {
+                "tech": "bemhtml",
+                "block": "i-bem"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "inherit"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "identify"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "next-tick"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "objects"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "functions"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "events"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "events",
+                "elem": "channels"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "ecma",
+                "elem": "object"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "ecma",
+                "elem": "array"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "ecma",
+                "elem": "function"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "i-bem"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "events"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "events",
+                "elem": "channels"
+            }
+        ]
+    }
+};
